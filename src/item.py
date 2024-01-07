b0ad1e094e890c48a19fc21e69acb77c489fa839
V_A_N <V_A_N-1_94@mail.ru>
2024-01-07 22:20:36 +0300

    HW - 2

--- src/item.py
+++ src/item.py
@@ -1,8 +1,11 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
     """
-    pay_rate: float = 1.0
+    pay_rate: float = 0.9
     all: list = []
 
     def __init__(self, name: str, price: float, quantity: int) -> None:
@@ -13,10 +16,25 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         Item.all.append(self)
+
+    @property
+    def name(self) -> str:
+        return self.__name
+
+    @name.setter
+    def name(self, value) -> None:
+        try:
+            if len(value) < 10:
+                self.__name = value
+            else:
+                raise Exception('Длина наименования товара превышает 10 символов.')
+        except:
+            print('Exception: Длина наименования товара превышает 10 символов.')
+            self.__name = value[:10]
 
     def calculate_total_price(self) -> float:
         """
@@ -31,3 +49,29 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= Item.pay_rate
+
+    @classmethod
+    def instantiate_from_csv(cls, path: str) -> list:
+        """
+        Создание экземпляра класса item из данных файла.
+
+        :param path: Путь к файлу с данными по товарам.
+        """
+        with open(path, 'r') as f:
+            reader = csv.DictReader(f)
+            for read in reader:
+                name = read['name']
+                price = float(read['price'])
+                quantity = int(read['quantity'])
+                cls(name, price, quantity)
+            return cls.all
+
+    @staticmethod
+    def string_to_number(string: str) -> float:
+        """
+        Преобразование строки в число.
+
+        :param string: Преобразованная строка.
+        :return: Преобразованное число.
+        """
+        return int(float(string))
--- tests/test_item.py
+++ tests/test_item.py
@@ -8,3 +8,21 @@ def test_calculate_total_price():
     Item.pay_rate = 0.8
     item1.apply_discount()
     assert item1.price == 8000.0
+    item1.name = 'СуперСмартфон'
+    assert item1.name == 'СуперСмарт'
+
+
+def test_instantiate_from_csv():
+    Item.all = []
+    items = Item.instantiate_from_csv('../src/items.csv')
+    assert len(items) == 5
+    assert items[0].name == 'Смартфон'
+    assert items[1].name == 'Ноутбук'
+    assert items[2].name == 'Кабель'
+    assert items[3].price == 50.0
+
+
+def test_string_to_number():
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('4.0') == 4
+    assert Item.string_to_number('3.6') == 3
